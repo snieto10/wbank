6f49a21beaa6b6a6a17e1d949cf5d5847cfd7b96
Sergio Nieto <snieto10@gmail.com>
2022-10-25 15:29:36 -0400

    improving menu

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -6,6 +6,7 @@ import { ReactComponent as Search } from "../images/searchnav.svg";
 class NavBar extends Component {
   state = {
     mobileMenu: false,
+    menu1: false,
   };
 
   handleMenuClick = () => {
@@ -13,10 +14,17 @@ class NavBar extends Component {
     this.setState({ mobileMenu });
   };
 
+  handleMenu1Click = () => {
+    let menu1 = !this.state.menu1;
+    this.setState({ menu1 });
+  };
+
   render() {
     let popMenu = this.state.mobileMenu
       ? "nav__options nav__options-active"
       : "nav__options";
+
+    let menu1 = this.state.menu1 ? "nav1 nav1-active" : "nav1";
 
     return (
       <>
@@ -79,6 +87,46 @@ class NavBar extends Component {
                   Education & Goals
                 </a>
               </li>
+            </ul>
+          </div>
+          <div>
+            <ul className="nav-bottom">
+              <li onClick={this.handleMenu1Click} className="nav-bottom-list">
+                Checking & Savings
+                <div className={menu1}>
+                  <ul className="listMenu">
+                    <li className="listlibottom">
+                      <a className="nav__menu" href="#">
+                        Checking Account
+                      </a>
+                    </li>
+                    <li className="listlibottom">
+                      <a className="nav__menu" href="#">
+                        Credits Cards
+                      </a>
+                    </li>
+                    <li className="listlibottom">
+                      <a className="nav__menu" href="#">
+                        Home Loans
+                      </a>
+                    </li>
+                    <li className="listlibottom">
+                      <a className="nav__menu" href="#">
+                        Auto
+                      </a>
+                    </li>
+                    <li className="listlibottom">
+                      <a className="nav__menu" href="#">
+                        Education & Goals
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="nav-bottom-list">Credit Cards</li>
+              <li className="nav-bottom-list">Home Loans</li>
+              <li className="nav-bottom-list">Auto</li>
+              <li className="nav-bottom-list">Financial Education</li>
             </ul>
           </div>
         </div>
